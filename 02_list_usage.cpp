c84e58a54c9befcc80bc61fc971731952a815d91
Timofey Khirianov <tkhirianov@gmail.com>
2020-09-02 15:08:52 +0300

    Лекция №1 осенью по С++

---
+++ 02_list_usage.cpp
@@ -0,0 +1,43 @@
+#include <list>
+#include <iostream>
+#include <algorithm>
+
+void procedure(int x)
+{
+    std::cout << x << '\n';
+}
+
+
+int main()
+{
+    std::list<int> A;
+
+    for(int i = 0; i < 10; i++)
+        A.push_back(i);
+    
+    // НЕУКЛЮЖАЯ ВЕРСИЯ:
+    std::list<int>::iterator it1 = A.begin();
+    while (it1 != A.end()) {
+        std::cout << *it1 << "\n";
+        ++it1;
+    }
+    // почти УКЛЮЖАЯ ВЕРСИЯ:
+    auto it2 = A.begin();
+    while (it2 != A.end()) {
+        std::cout << *it2 << "\n";
+        ++it2;
+    }
+    for (auto it3 = A.begin(); it3 != A.end(); ++it3) {
+        std::cout << *it3 << "\n";
+    }
+
+    std::cout << "for_each:\n";
+    std::for_each(A.begin(), A.end(), procedure);
+
+    std::cout << "range based for:\n";
+    for (auto x: A) {
+        std::cout << x << ' ';
+    }
+
+    return 0;
+}
