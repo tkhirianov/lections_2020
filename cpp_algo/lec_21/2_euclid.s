0b667279d5df4c6b9c1cb3e345b274ecba7aaaa6
Timofey Khirianov <tkhirianov@gmail.com>
2020-11-04 14:59:20 +0300

    stages of compilation

---
+++ cpp_algo/lec_21/2_euclid.s
@@ -0,0 +1,160 @@
+	.file	"2_euclid.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.text
+	.globl	_Z10euclid_gcdii
+	.type	_Z10euclid_gcdii, @function
+_Z10euclid_gcdii:
+.LFB1518:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+.L5:
+	movl	-4(%rbp), %eax
+	cmpl	-8(%rbp), %eax
+	je	.L2
+	movl	-4(%rbp), %eax
+	cmpl	-8(%rbp), %eax
+	jle	.L3
+	movl	-8(%rbp), %eax
+	subl	%eax, -4(%rbp)
+	jmp	.L5
+.L3:
+	movl	-4(%rbp), %eax
+	subl	%eax, -8(%rbp)
+	jmp	.L5
+.L2:
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1518:
+	.size	_Z10euclid_gcdii, .-_Z10euclid_gcdii
+	.section	.rodata
+.LC0:
+	.string	"Enter integer. x = "
+.LC1:
+	.string	"Enter integer. y = "
+.LC2:
+	.string	"GCD(x, y) = "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1519:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset 3, -24
+	leaq	.LC0(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC1(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC2(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rbx
+	movl	-24(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	_Z10euclid_gcdii
+	movl	%eax, %esi
+	movq	%rbx, %rdi
+	call	_ZNSolsEi@PLT
+	movq	%rax, %rdx
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	movl	$0, %eax
+	addq	$24, %rsp
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1519:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2008:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L11
+	cmpl	$65535, -8(%rbp)
+	jne	.L11
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L11:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2008:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z10euclid_gcdii, @function
+_GLOBAL__sub_I__Z10euclid_gcdii:
+.LFB2009:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2009:
+	.size	_GLOBAL__sub_I__Z10euclid_gcdii, .-_GLOBAL__sub_I__Z10euclid_gcdii
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z10euclid_gcdii
+	.hidden	__dso_handle
+	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
